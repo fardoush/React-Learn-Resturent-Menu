59154910af3dba638dc10e7ac477e04f7a1d516e
Your User Name <fardoushjahanara@gmail.com>
2022-11-16 22:26:33 +0600

    use to menu card in props, menu Api and use to usestate hookusing map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Resturent from './Components/Basic/Resturent';
+
 
 const App = () => {
   return (
     <>
       <h1>Resturent Menu</h1>
+      <Resturent/>
+      
     </>
   )
 }
--- src/Components/Basic/Resturent.js
+++ src/Components/Basic/Resturent.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./style.css";
+import Menu from './MenuApi';
+import MenuCard from "./MenuCard";
+
 
 const Resturent = () => {
+    const [menuData,setMenuData]=useState(Menu);
   return (
     <>
-      
+   <MenuCard menuData={menuData}/>
+
+
     </>
   )
 }
